bb90a56a9b468fcfd38b695ee6399b921055b9b1
Vinícius Botelho <viniciusobotelho@hotmail.com>
2025-05-11 12:15:32 -0300

    Demand estimation

--- scripts/functions/find_delta_teste.R
+++ scripts/functions/find_delta_teste.R
@@ -16,11 +16,10 @@ find_delta <- function(MERCADO, M, temp){
   contraction <- function(DELTA_TRY){
     V <- A_matrix * exp(kronecker(matrix(1, nrow = nrow(F_matrix), ncol = 1), t(DELTA_TRY)) + M)
     Vaux <- diag(as.vector(V %*% matrix(1, nrow = nrow(DELTA_TRY), ncol = 1) + matrix(1, nrow = nrow(F_matrix), ncol = 1)))
-    system.time(S <- chol2inv(Vaux) %*% V)
-    system.time(S <- solve(Vaux) %*% V)
+    S <- solve(Vaux) %*% V
     SHARES <- t(S) %*% F_matrix
     NEW_DELTA <- DELTA_TRY + log(TARGET_matrix) - log(SHARES)
-    return(NEW_DELTA)
+    return(as.matrix(NEW_DELTA))
   }
   
   RESULT <- squarem(DELTA_START, contraction, control = list(tol = 1e-12, trace = TRUE))
